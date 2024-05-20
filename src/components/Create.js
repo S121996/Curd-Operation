c253f7f1875f491bcd23cc8efabd4a24b4ddbfcb
DrumKit <sumitbhargav12@gmail.com>
2024-05-20 16:51:16 +0530

    Read/Delete components

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
+
 const Create = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
   const header = { "Access-Control-Allow-origin": "*" };
-
+  const history =useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("click")
     axios.post("https://664b1152a300e8795d441633.mockapi.io/crudoperation", {
       name: name,
       email: email,
+      contact:contact,
       header,
-    });
+    })
+    .then(()=>{
+      history("/read");
+    })
   };
   return (
     <>
@@ -33,6 +41,15 @@ const Create = () => {
             className="form-control"
             aria-describedby="emailHelp"
             onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Contact</label>
+          <input
+            type="number"
+            className="form-control"
+            aria-describedby="numberHelp"
+            onChange={(e) => setContact(e.target.value)}
           />
         </div>
 
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Read = () => {
-  return (
-    <div>Read</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Read
+  function getData() {
+    axios
+      .get("https://664b1152a300e8795d441633.mockapi.io/crudoperation")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      });
+  }
+
+  function handleDelete(id) {
+    axios
+    .delete(`https://664b1152a300e8795d441633.mockapi.io/crudoperation${id}`)
+    .then(
+        () => {
+          getData();
+        }
+      )
+    
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h2>Read Opertaion</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Contact</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        {data.map((eachData) => {
+          return (
+            <>
+              <tbody>
+                <tr>
+                  <th scope="row">{eachData.id}</th>
+                  <td>{eachData.name}</td>
+                  <td>{eachData.email}</td>
+                  <td>{eachData.contact}</td>
+                  <td>
+                    <button className="btn-success">Edit</button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn-danger"
+                      onClick={() => handleDelete(eachData.id)}
+                    >
+                      Delete{" "}
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </>
+          );
+        })}
+      </table>
+    </>
+  );
+};
+
+export default Read;
